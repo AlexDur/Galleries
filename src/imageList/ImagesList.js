b1900d844afa1320178fa679d2f41dfa1ded2b14
Alusch <alexdurach@gmail.com>
2022-03-25 02:19:56 +0100

    added lightbox

--- src/imageList/ImagesList.js
+++ src/imageList/ImagesList.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
+import SimpleReactLightbox, { SRLWrapper } from "simple-react-lightbox";
 
 import pic1 from "../img/pic002.png";
 import pic2 from "../img/pic_0178.png";
@@ -12,7 +13,7 @@ import pic7 from "../img/pic064.png";
 import pic8 from "../img/bach.png";
 /* import pic9 from "../img/pic_4282.png" */
 import pic10 from "../img/pic_1911.png";
-import pic11 from "../img/pic4.png";
+import pic11 from "../img/blue.png";
 import pic12 from "../img/pic111.png";
 
 function srcset(image, size, rows = 1, cols = 1) {
@@ -26,85 +27,86 @@ function srcset(image, size, rows = 1, cols = 1) {
 
 export default function QuiltedImageList() {
   return (
-    <ImageList
-      variant="quilted"
-      cols={4}
-      rowWidth={200}
-      style={{ paddingBottom: 100 }}
-    >
-      {itemData.map(item => (
-        <ImageListItem
-          key={item.img}
-          cols={item.cols || 1}
-          rows={item.rows || 1}
+    <SimpleReactLightbox>
+      <SRLWrapper>
+        <ImageList
+          variant="quilted"
+          cols={4}
+          rowWidth={200}
+          style={{ paddingBottom: 100 }}
         >
-          <img
-            {...srcset(item.img, 121, item.rows, item.cols)}
-            alt={item.title}
-            loading="lazy"
-          />
-        </ImageListItem>
-      ))}
-    </ImageList>
+          {itemData.map(item => (
+            <ImageListItem
+              key={item.img}
+              cols={item.cols || 1}
+              rows={item.rows || 1}
+            >
+              <img
+                {...srcset(item.img, 121, item.rows, item.cols)}
+                alt={item.title}
+                loading="lazy"
+              />
+            </ImageListItem>
+          ))}
+        </ImageList>
+      </SRLWrapper>
+    </SimpleReactLightbox>
   );
 }
 
 const itemData = [
   {
     img: pic1,
-    title: "pic1",
+    title: "wood",
     rows: 2,
     cols: 2,
   },
   {
     img: pic5,
-    title: "pic5",
+    title: "evening sun",
   },
   {
     img: pic4,
-    title: "pic4",
+    title: "juicy green",
   },
   {
     img: pic6,
-    title: "pic6",
+    title: "evening sun 2",
     cols: 2,
   },
   {
     img: pic2,
-    title: "pic2",
+    title: "view2",
     cols: 2,
   },
   {
     img: pic3,
-    title: "pic3",
+    title: "bridge over creek",
     rows: 2,
     cols: 2,
   },
   {
     img: pic7,
-    title: "pic7",
+    title: "dead tree with a view",
   },
   {
     img: pic8,
-    title: "pic8",
+    title: "bridge over creek",
   },
   {
     img: pic10,
-    title: "Mushrooms",
+    title: "slightly sloping on the bike",
     rows: 2,
     cols: 2,
   },
   {
-    img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
-    title: "pic10",
-  },
-  {
     img: pic11,
-    title: "pic11",
+    title: "blue mountains",
+    cols: 2,
   },
   {
     img: pic12,
-    title: "",
+    title: "shadow and sun valley",
     cols: 2,
   },
 ];
--- src/ui/Nav.js
+++ src/ui/Nav.js
@@ -32,6 +32,7 @@ export default function Nav() {
   });
   return (
     <React.Fragment>
+      <h1>From the Black Forest</h1>
       <Box sx={{ display: "flex", alignItems: "center", textAlign: "center" }}>
         {!currentUser ? (
           <Button startIcon={<Terrain />}>Login</Button>
